4a5f67ee5ca0a0a8eb18b9c78f5e94cac0186c42
valentineezeh <val.ezeh15@gmail.com>
2021-03-20 06:05:36 +0100

    implement get products api call

--- lumin/src/components/ProductSegment/ProductSegment.style.ts
+++ lumin/src/components/ProductSegment/ProductSegment.style.ts
@@ -2,9 +2,38 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export const ProductSegmentStyle = makeStyles((theme) => ({
   root: {
-    display: "flex",
+    // margin: "auto",
     width: "100%",
-    height: 600,
+    height: "100%",
     background: "#e3e6e3",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "128px",
+    height: "128px",
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+  cartButton: {
+    textTransform: "none",
+    background: "#51594f !important",
+    height: 50,
+    padding: "0 30px",
+    borderRadius: 0,
+    color: "#ffff",
+    marginTop: theme.spacing(2),
+  },
+  titleText: {
+    fontFamily: "Karla",
+    fontSize: 16,
+    fontStyle: "bold",
+  },
+  productContainer: {
+    marginTop: 50,
   },
 }));
